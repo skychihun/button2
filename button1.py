f92f3cc41b6687cb67fe8ce68d1adcf7e9c8be77
ChiHeon Kwon <5chihun@naver.com>
2017-11-15 13:35:29 +0900

    birthday

--- button1.py
+++ button1.py
@@ -4,8 +4,10 @@ window =Tk()
 
 b1 = Button(window, text="KwonChiHeon")
 b2 = Button(window, text="60132228")
+b3 = Button(window, text="1/6")
 
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
+b3.grid(row=1, column=0)
 
 window.mainloop()
